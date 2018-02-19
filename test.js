d158687612a50852f1368f7a3d36f97e3621ee58
Harry Lachenmayer <harrylachenmayer@gmail.com>
2018-02-19 17:58:52 +0000

    add some tests

--- test.js
+++ test.js
@@ -1,6 +1,33 @@
-var test = require('tape')
+var test = require('ava')
 
-test('placeholder', function (t) {
-  t.ok(true)
-  t.end()
+var hypha = require('.')
+
+test('readPageSync works', function (t) {
+  var page = hypha.readPageSync('example/content/about')
+  t.is(page.title, 'About')
+  t.is(page.view, 'custom')
+})
+
+test('readPage works', async function (t) {
+  var page = await hypha.readPage('example/content/about')
+  t.is(page.title, 'About')
+  t.is(page.view, 'custom')
+})
+
+test('readPageSync and readPage outputs are the same', async function (t) {
+  var sync = hypha.readPageSync('example/content/about')
+  var async = await hypha.readPage('example/content/about')
+  t.deepEqual(sync, async)
+})
+
+test('readSiteSync works', function (t) {
+  var site = hypha.readSiteSync('example/content')
+  t.is(site['/'].title, 'Example')
+  t.is(site['/about'].title, 'About')
+})
+
+test('readSiteSync and readSite outputs are the same', async function (t) {
+  var sync = hypha.readSiteSync('example/content')
+  var async = await hypha.readSiteSync('example/content')
+  t.deepEqual(sync, async)
 })
