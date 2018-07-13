0549fefc53960344fa68fd463854400c48408357
jondashkyle <contact@jon-kyle.com>
2018-07-13 13:07:53 -0700

    fixes transform

--- transform.js
+++ transform.js
@@ -2,7 +2,7 @@ var staticModule = require('static-module')
 var through = require('through2')
 var path = require('path')
 
-var hypha = require('.')
+var nanocontent = require('.')
 
 module.exports = transform
 
@@ -15,13 +15,14 @@ function transform (filename) {
   }
 
   var sm = staticModule({
-    hypha: {
+    nanocontent: {
       readPageSync: function (pathPage, opts) {
         opts = opts || { }
         opts.pathRoot = opts.pathRoot || vars.__dirname
+        opts.parent = typeof opts.parent !== 'undefined' ? opts.parent : true
 
         var pathDir = path.isAbsolute(pathPage) ? pathPage : path.join(vars.__dirname, pathPage)
-        var pageSync = hypha.readPageSync(pathDir, opts)
+        var pageSync = nanocontent.readPageSync(pathDir, opts)
 
         var stream = through()
         stream.push(JSON.stringify(pageSync, { }, 2))
@@ -31,13 +32,14 @@ function transform (filename) {
       readSiteSync: function readDir (pathSite, opts) {
         opts = opts || { }
         opts.pathRoot = opts.pathRoot || vars.__dirname
+        opts.parent = typeof opts.parent !== 'undefined' ? opts.parent : true
 
         opts.onFile = function (pathFile) {
           sm.emit('file', pathFile)
         }
 
         var pathDir = path.isAbsolute(pathSite) ? pathSite : path.join(vars.__dirname, pathSite)
-        var siteSync = hypha.readSiteSync(pathDir, opts)
+        var siteSync = nanocontent.readSiteSync(pathDir, opts)
 
         var stream = through()
         stream.push(JSON.stringify(siteSync, { }, 2))
